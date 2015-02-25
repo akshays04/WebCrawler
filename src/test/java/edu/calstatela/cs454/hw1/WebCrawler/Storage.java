9b5eb05456b903cc325d1cec989176a1523263a2
shreyasDarwhatkar <shreyas.darwhatkar@hotmail.com>
2015-02-25 15:00:06 -0800

    create new folder and downloading file

--- src/test/java/edu/calstatela/cs454/hw1/WebCrawler/Storage.java
+++ src/test/java/edu/calstatela/cs454/hw1/WebCrawler/Storage.java
@@ -1,5 +1,6 @@
 package edu.calstatela.cs454.hw1.WebCrawler;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,12 +26,21 @@ public class Storage {
 	public static UUID save(String url) {
 		UUID uuid = null;
 		try{
+			if(!url.toLowerCase().contains("https"))
+			{
+				uuid = UUID.randomUUID();
+			File directory = new File(".\\CrawlerStorage\\"+uuid.toString());
+			if (directory.mkdir()) {
+				System.out.println("Directory is created!");
+			} else {
+				System.out.println("Failed to create directory!");
+			}
 			//creating new obj
 			DataWeb data = new DataWeb();
 			
 			//adding url
 			data.setUrl(url);
-			uuid = UUID.randomUUID();
+			
 			
 			
 			//adding doc data
@@ -52,17 +62,26 @@ public class Storage {
 			{
 				URL website = new URL(url);
 				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-				FileOutputStream fos = new FileOutputStream(uuid.toString()+".pdf");
+				FileOutputStream fos = new FileOutputStream(".\\CrawlerStorage\\"+uuid.toString()+"\\"+uuid.toString()+".pdf");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			}
+			if(true)
+			{
+				URL website = new URL(url);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream(".\\CrawlerStorage\\"+uuid.toString()+"\\"+uuid.toString()+".html");
 				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 			}
 			
+			
 			System.out.println(uuid);
-			FileWriter file = new FileWriter(".\\CrawlerStorage\\"+uuid+".json");
+			FileWriter file = new FileWriter(".\\CrawlerStorage\\"+uuid.toString()+"\\"+uuid+".json");
 			file.write(data.getJson().toJSONString());
 			file.write("\r\n");
 			
 			//file.flush();
 			file.close();
+			}
 			
 		}catch(Exception e){e.printStackTrace();}
 		return uuid;
